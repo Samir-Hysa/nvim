efd7c1a17f91b796f3a2018806edd63d39a5ed87
samir.hysa <samir.hysa@deltatre.com>
2023-09-02 17:54:54 +0200

    First commit

---
+++ lua/shuun/init.lua
@@ -0,0 +1,15 @@
+require("shuun.remap")
+require("shuun.set")
+require("shuun.packer")
+-- Load Telescope
+local telescope = require('telescope')
+
+-- Configure Telescope
+telescope.setup {
+  defaults = {
+    -- Other default options...
+
+    -- Show hidden files
+    find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
+  }
+}
